9c8cdc6daf1c16e7add999f71e55c74dca3fbb72
Huguez <carlos.huguez@gmail.com>
2020-07-12 19:07:21 -0700

    se creo un services para utilizarlo con la api

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private type_API: string    = "https://pokeapi.co/api/v2/type";
+  private pokemon_API: string = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=200";
+  
+  constructor( private http:HttpClient ) { }
+
+  getTypes(){
+    return this.http.get<any>( this.type_API );
+  }
+  
+  getPokemons(){
+    return this.http.get<any>( this.pokemon_API );
+  }
+
+}
