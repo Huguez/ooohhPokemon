090eee309bc2ee74245936513e9da3dcd80bd0ac
Huguez <carlos.huguez@gmail.com>
2020-07-12 19:11:04 -0700

    se despliegan los pokemons con poco de formato

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { Resp } from 'src/app/interfaces/resp';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  public array: any[] = []; 
 
-  constructor() { }
-
+  constructor( private _ps: PokemonService ) { }
+// { count: number, next: any, previous: any, results: Array<any> }
   ngOnInit(): void {
+    this._ps.getPokemons().subscribe( ( p: Resp ) =>{
+      // const keys = Object.keys( p )      
+      // console.log(keys);
+      this.array = p.results;
+    });
   }
+  
+  
+
 
 }
