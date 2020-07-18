598693c5440f512a6f00991372723804e7a6d128
Huguez <carlos.huguez@gmail.com>
2020-07-17 21:23:39 -0700

    se hicieron avances en el componente tipo

--- src/app/components/tipo/tipo.component.ts
+++ src/app/components/tipo/tipo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
 import { InterfacePokemon } from '../../interfaces/interface-pokemon';
+import { DamageRelation } from '../../interfaces/damage-relation';
 
 @Component({
   selector: 'app-tipo',
@@ -12,8 +13,10 @@ export class TipoComponent implements OnInit {
   
   private id : number = -1;
   public titulo = "";
-  public damageRelations: any;
+  public damageRelations: DamageRelation;
   
+  public carga: boolean;
+
   public gameIndices: any;
   public generation: any;
   public moveDamageClass: any;
@@ -21,27 +24,33 @@ export class TipoComponent implements OnInit {
   public names: any;
   public pokemons: any;
 
+  public prueba : any;
+
   constructor( private _ps : PokemonService , private actRou: ActivatedRoute ) {
     const param = this.actRou.snapshot.paramMap.get("id");
+    this.carga = false;
     this.id = parseInt( param, 10 );
   }
 
   ngOnInit(): void {
     this._ps.getTipo( this.id ).subscribe( (pokebato: InterfacePokemon) => { 
-      // this.pokemon = pokebato;
+      
       this.titulo =  pokebato.name ;
-      this.damageRelations = pokebato.damage_relations ;
-      // console.log( this.damageRelations );
+
+      this.damageRelations = pokebato.damage_relations;
       
       this.gameIndices = pokebato.game_indices;
-
-      this.generation  = pokebato.generation ;
+      
       this.moveDamageClass  = pokebato.move_damage_class ;
+      
       this.moves  = pokebato.moves ;
+      
       this.names  = pokebato.names ;
+      
       this.pokemons = pokebato.pokemon.map( elem => { return elem.pokemon.name  } ) ;
-
-     });
+      
+     this.carga = true;
+    });
   }
 
 }
