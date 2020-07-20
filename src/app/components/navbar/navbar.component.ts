d94987fba8ea8cbb4380d0bca333289ee7368664
Huguez <carlos.huguez@gmail.com>
2020-07-19 18:58:56 -0700

    se creo modifico el navbar para hacer pruebas con las rutas de generacio

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  
+  public listaGeneracion: Array<any> = [];
 
-  constructor() { }
+  constructor( private _ps:PokemonService,private router: Router, private actRou: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    
+    this._ps.getAllGeneraciones().subscribe( ( lg : any ) => {
+      this.listaGeneracion = lg.results.map( item => { 
+        
+        const arreglo = item.url.split('/');
+        
+        return { name: item.name, id: arreglo[6] };
+       } );
+    } );
+  }
+
+  reload( id: number ){
+    
   }
 
 }
