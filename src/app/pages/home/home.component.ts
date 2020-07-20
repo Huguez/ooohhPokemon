b1c7831538c10c2a33294da845ca8b11d5d2cbcd
Huguez <carlos.huguez@gmail.com>
2020-07-19 22:23:45 -0700

    se agrego el services de un pokemon individual para poder tomas su imagen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,17 +9,60 @@ import { Resp } from '../../interfaces/resp';
 })
 export class HomeComponent implements OnInit {
   
-  public array: any[] = []; 
+  public array:any[] = []; 
+  public carga:boolean = false;
+  private imagenAux:string = "";
 
   constructor( private _ps: PokemonService ) { }
-// { count: number, next: any, previous: any, results: Array<any> }
-  ngOnInit(): void {
-    this._ps.getPokemons().subscribe( ( p: Resp ) =>{
-      this.array = p.results;
-    });
-  }
-  
-  
 
+  ngOnInit(): void {
+    
+    this._ps.getPokemons().subscribe( ( p: Resp ) =>{
+        
+      this.array = p.results.map( (item:any) => {
+        return { name: item.name, img: "" };
+      });
+      
+      this.array.forEach( item =>{
+      
+        this._ps.getPokemonName( item.name ).subscribe( p =>{
+          
+          item.img = p.sprites.front_default
+  
+        } );
+  
+  
+      });
+  
+      this.carga = true;
+    });
+
+  }
 
 }
+
+
+// this._ps.getPokemonName( item.name ).subscribe( p =>{  
+          
+//   console.log( p.sprites );
+
+//   if ( p.sprites.front_default != null ) {
+//     imagen = p.sprites.front_default;
+
+//   } else {
+//     if( p.sprites.front_female != null ){
+//       imagen = p.sprites.front_female;
+//     } else {
+//       if( p.sprites.front_shiny != null  ){
+//         imagen = p.sprites.front_shiny;
+//       }else{
+//         if( p.sprites.front_shiny_female != null ){
+//           imagen = p.sprites.front_shiny_female;
+//         }else{
+//           imagen = "";
+//         }
+//       }
+//     } 
+//   }
+  
+// });
